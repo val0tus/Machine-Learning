a6e7e47edea1fc9497a8a95c6065007fa05f1c65
Harri Valkonen <34934699+val0tus@users.noreply.github.com>
2020-04-24 13:36:05 +0300

    Add files via upload

---
+++ Flickr CIRAF 10/jpg2png.py
@@ -0,0 +1,17 @@
+#https://stackoverflow.com/questions/10759117/converting-jpg-images-to-png
+
+from PIL import Image
+from os import listdir
+from os.path import splitext
+
+target_directory = '.'
+target = '.png'
+
+for file in listdir(target_directory):
+    filename, extension = splitext(file)
+    try:
+        if extension not in ['.py', target]:
+            im = Image.open(filename + extension)
+            im.save(filename + target)
+    except OSError:
+        print('Cannot convert %s' % file)
